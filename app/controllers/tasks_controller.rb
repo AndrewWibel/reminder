a5f2a8ae5d36a3ff270cb8622cacb79458cf6108
Andrew Wibel <andrew.wibel@gmail.com>
2016-02-18 12:26:07 -0800

    added some scheduled jobs

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,5 @@
+class TasksController < ApplicationController
+	def 2mins
+		puts 'CHANGED THIS THINGS!'
+	end
+end
---
+++ app/models/task.rb
@@ -0,0 +1,5 @@
+class Task < ActiveRecord::Base
+	def self.every_2mins
+		puts 'CHANGED THIS '
+	end
+end
